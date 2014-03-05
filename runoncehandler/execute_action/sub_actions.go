2a2d419bc1888eb6e6d1db3406824f5addc800fc
Abhijit Hiremagalur <abhi@pivotallabs.com>
2014-03-05 15:23:16 -0800

    Introduce execute_action.SubActions
    - Makes a list of actions that ExecuteAction will eventually use
    - Not yet wired in
    
    Signed-off-by: Onsi Fakhouri <onsi@pivotallabs.com>

---
+++ runoncehandler/execute_action/sub_actions.go
@@ -0,0 +1,78 @@
+package execute_action
+
+import (
+	"github.com/cloudfoundry-incubator/executor/action_runner"
+	"github.com/cloudfoundry-incubator/executor/actionrunner/downloader"
+	"github.com/cloudfoundry-incubator/executor/actionrunner/logstreamer"
+	"github.com/cloudfoundry-incubator/executor/actionrunner/uploader"
+	"github.com/cloudfoundry-incubator/executor/backend_plugin"
+	"github.com/cloudfoundry-incubator/executor/runoncehandler/execute_action/download_action"
+	"github.com/cloudfoundry-incubator/executor/runoncehandler/execute_action/fetch_result_action"
+	"github.com/cloudfoundry-incubator/executor/runoncehandler/execute_action/run_action"
+	"github.com/cloudfoundry-incubator/executor/runoncehandler/execute_action/upload_action"
+	"github.com/cloudfoundry-incubator/runtime-schema/models"
+	steno "github.com/cloudfoundry/gosteno"
+	"github.com/vito/gordon"
+)
+
+func SubActions(
+	actionModels []models.ExecutorAction,
+	runOnce *models.RunOnce,
+	containerHandle string,
+	streamer logstreamer.LogStreamer,
+	downloader downloader.Downloader,
+	uploader uploader.Uploader,
+	backendPlugin backend_plugin.BackendPlugin,
+	wardenClient gordon.Client,
+	logger *steno.Logger,
+	tempDir string,
+
+) []action_runner.Action {
+	subActions := []action_runner.Action{}
+	var subAction action_runner.Action
+	for _, a := range actionModels {
+		switch actionModel := a.Action.(type) {
+		case models.RunAction:
+			subAction = run_action.New(
+				actionModel,
+				containerHandle,
+				streamer,
+				backendPlugin,
+				wardenClient,
+				logger,
+			)
+		case models.DownloadAction:
+			subAction = download_action.New(
+				actionModel,
+				containerHandle,
+				downloader,
+				tempDir,
+				backendPlugin,
+				wardenClient,
+				logger,
+			)
+		case models.UploadAction:
+			subAction = upload_action.New(
+				actionModel,
+				containerHandle,
+				uploader,
+				tempDir,
+				wardenClient,
+				logger,
+			)
+		case models.FetchResultAction:
+			subAction = fetch_result_action.New(
+				runOnce,
+				actionModel,
+				containerHandle,
+				tempDir,
+				wardenClient,
+				logger,
+			)
+		}
+
+		subActions = append(subActions, subAction)
+	}
+
+	return subActions
+}
---
+++ runoncehandler/execute_action/sub_actions_test.go
@@ -0,0 +1,106 @@
+package execute_action_test
+
+import (
+	"github.com/cloudfoundry-incubator/executor/actionrunner/downloader"
+	"github.com/cloudfoundry-incubator/executor/actionrunner/downloader/fakedownloader"
+	"github.com/cloudfoundry-incubator/executor/actionrunner/logstreamer/fakelogstreamer"
+	"github.com/cloudfoundry-incubator/executor/actionrunner/uploader/fakeuploader"
+	"github.com/cloudfoundry-incubator/runtime-schema/models"
+	steno "github.com/cloudfoundry/gosteno"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/vito/gordon/fake_gordon"
+
+	"github.com/cloudfoundry-incubator/executor/action_runner"
+	"github.com/cloudfoundry-incubator/executor/actionrunner/uploader"
+	"github.com/cloudfoundry-incubator/executor/backend_plugin"
+	"github.com/cloudfoundry-incubator/executor/linuxplugin"
+	. "github.com/cloudfoundry-incubator/executor/runoncehandler/execute_action"
+	"github.com/cloudfoundry-incubator/executor/runoncehandler/execute_action/download_action"
+	"github.com/cloudfoundry-incubator/executor/runoncehandler/execute_action/fetch_result_action"
+	"github.com/cloudfoundry-incubator/executor/runoncehandler/execute_action/run_action"
+	"github.com/cloudfoundry-incubator/executor/runoncehandler/execute_action/upload_action"
+)
+
+var _ = Describe("SubActions", func() {
+	Describe("SubActions", func() {
+		var (
+			backendPlugin backend_plugin.BackendPlugin
+			downloader    downloader.Downloader
+			logger        *steno.Logger
+			streamer      *fakelogstreamer.FakeLogStreamer
+			uploader      uploader.Uploader
+			wardenClient  *fake_gordon.FakeGordon
+		)
+
+		BeforeEach(func() {
+			backendPlugin = linuxplugin.New()
+			downloader = &fakedownloader.FakeDownloader{}
+			uploader = &fakeuploader.FakeUploader{}
+			logger = &steno.Logger{}
+		})
+
+		FIt("is correct", func() {
+			runOnce := models.RunOnce{Guid: "some-guid"}
+
+			runActionModel := models.RunAction{Script: "do-something"}
+			downloadActionModel := models.DownloadAction{From: "/file/to/download"}
+			uploadActionModel := models.UploadAction{From: "/file/to/upload"}
+			fetchResultActionModel := models.FetchResultAction{File: "some-file"}
+
+			actionModels := []models.ExecutorAction{
+				{runActionModel},
+				{downloadActionModel},
+				{uploadActionModel},
+				{fetchResultActionModel},
+			}
+
+			Ω(SubActions(
+				actionModels,
+				&runOnce,
+				"some-container-handle",
+				streamer,
+				downloader,
+				uploader,
+				backendPlugin,
+				wardenClient,
+				logger,
+				"/fake/temp/dir",
+			)).To(Equal([]action_runner.Action{
+				run_action.New(
+					runActionModel,
+					"some-container-handle",
+					streamer,
+					backendPlugin,
+					wardenClient,
+					logger,
+				),
+				download_action.New(
+					downloadActionModel,
+					"some-container-handle",
+					downloader,
+					"/fake/temp/dir",
+					backendPlugin,
+					wardenClient,
+					logger,
+				),
+				upload_action.New(
+					uploadActionModel,
+					"some-container-handle",
+					uploader,
+					"/fake/temp/dir",
+					wardenClient,
+					logger,
+				),
+				fetch_result_action.New(
+					&runOnce,
+					fetchResultActionModel,
+					"some-container-handle",
+					"/fake/temp/dir",
+					wardenClient,
+					logger,
+				),
+			}))
+		})
+	})
+})
