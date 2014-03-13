da71b1642b08f2ed14b15312d3ab6d10c712f51b
Ted Young <tyoung@pivotallabs.com>
2014-03-13 12:18:50 -0700

    Extractor supports .tgz archives
    Signed-off-by: Aram Price <aprice@pivotallabs.com>

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -1,51 +1,135 @@
 package extractor
 
 import (
+	"archive/tar"
 	"archive/zip"
+	"compress/gzip"
+	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 )
 
-func Extract(src string, destination string) error {
+func Extract(src, dest string) error {
+	srcType, err := mimeType(src)
+	if err != nil {
+		return err
+	}
+
+	switch srcType {
+	case "application/zip":
+		err := extractZip(src, dest)
+		if err != nil {
+			return err
+		}
+	case "application/x-gzip":
+		err := extractTgz(src, dest)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("unsupported archive type: %s", srcType)
+	}
+
+	os.Remove(src)
+	return nil
+}
+
+func mimeType(src string) (string, error) {
+	fd, err := os.Open(src)
+	if err != nil {
+		return "", err
+	}
+	defer fd.Close()
+
+	data := make([]byte, 512)
+
+	_, err = fd.Read(data)
+	if err != nil {
+		return "", err
+	}
+
+	return http.DetectContentType(data), nil
+}
+
+func extractTgz(src, dest string) error {
+	fd, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	gReader, err := gzip.NewReader(fd)
+	if err != nil {
+		return err
+	}
+	defer gReader.Close()
+
+	tarReader := tar.NewReader(gReader)
+
+	for {
+		hdr, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		err = extractArchiveFile(filepath.Join(dest, hdr.Name), tarReader, hdr.FileInfo())
+		if err != nil {
+			return err
+		}
+
+	}
+	return nil
+}
+
+func extractZip(src, dest string) error {
 	files, err := zip.OpenReader(src)
 	if err != nil {
 		return err
 	}
 	defer files.Close()
 
-	// We loop over all files in the zip archive
-	for _, file := range files.File { //ignore the index
-		// Always make the directory for the file
-		path := filepath.Join(destination, file.Name)
-		err = os.MkdirAll(filepath.Dir(path), os.ModeDir|os.ModePerm)
-		if err != nil {
-			return err
-		}
-
-		//Open the file for reading
-		readCloser, err := file.Open()
-		if err != nil {
-			return err
-		}
-
-		// If it's a file, write out the file
-		if !file.FileInfo().IsDir() {
-			fileCopy, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, file.FileInfo().Mode())
+	for _, file := range files.File {
+		err = func() error {
+			readCloser, err := file.Open()
 			if err != nil {
 				return err
 			}
-			_, err = io.Copy(fileCopy, readCloser)
-			if err != nil {
-				return err
-			}
-			fileCopy.Close()
-		}
+			defer readCloser.Close()
 
-		//Tidy up!  It's important to do this in the loop, so that we don't run out of file descriptors on large archives.
-		readCloser.Close()
+			return extractArchiveFile(filepath.Join(dest, file.Name), readCloser, file.FileInfo())
+		}()
+
+		if err != nil {
+			return err
+		}
 	}
 
-	os.Remove(src)
+	return nil
+}
+
+func extractArchiveFile(filePath string, input io.Reader, fileInfo os.FileInfo) error {
+	err := os.MkdirAll(filepath.Dir(filePath), os.ModeDir|os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	if !fileInfo.IsDir() {
+		fileCopy, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, fileInfo.Mode())
+		if err != nil {
+			return err
+		}
+		defer fileCopy.Close()
+
+		_, err = io.Copy(fileCopy, input)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
--- extractor/extractor_test.go
+++ extractor/extractor_test.go
@@ -12,56 +12,85 @@ import (
 )
 
 var _ = Describe("Extractor", func() {
-	var destination string
-	var fixturetmp string
-	var fixture string
+	var extractionDest string
+	var extractionSrc string
+	var tempDir string
+	var archiveFixture string
 
-	BeforeEach(func() {
+	JustBeforeEach(func() {
 		var err error
 
-		fixturetmp, err = ioutil.TempDir("", "extractor-fixture")
+		tempDir, err = ioutil.TempDir("", "extractor-fixture")
 		Ω(err).ShouldNot(HaveOccurred())
 
-		fixture = filepath.Join(fixturetmp, "fixture.zip")
+		extractionSrc = filepath.Join(tempDir, archiveFixture)
 
-		err = exec.Command("cp", "../fixtures/fixture.zip", fixture).Run()
+		err = exec.Command("cp", "../fixtures/"+archiveFixture, extractionSrc).Run()
 		Ω(err).ShouldNot(HaveOccurred())
 
-		destination, err = ioutil.TempDir(os.TempDir(), "extracted")
+		extractionDest, err = ioutil.TempDir(os.TempDir(), "extracted")
 		Ω(err).ShouldNot(HaveOccurred())
 	})
 
 	AfterEach(func() {
-		os.RemoveAll(destination)
-		os.RemoveAll(fixturetmp)
+		os.RemoveAll(extractionDest)
+		os.RemoveAll(tempDir)
 	})
 
-	It("should extract zip files, generating directories, and honoring file permissions", func() {
-		err := Extract(fixture, destination)
+	var extractionTest = func() {
+		err := Extract(extractionSrc, extractionDest)
 		Ω(err).ShouldNot(HaveOccurred())
 
-		fileContents, err := ioutil.ReadFile(filepath.Join(destination, "fixture", "file"))
+		fileContents, err := ioutil.ReadFile(filepath.Join(extractionDest, "fixture", "file"))
 		Ω(err).ShouldNot(HaveOccurred())
 		Ω(string(fileContents)).Should(Equal("I am a file"))
 
-		fileContents, err = ioutil.ReadFile(filepath.Join(destination, "fixture", "iamadirectory", "another_file"))
+		fileContents, err = ioutil.ReadFile(filepath.Join(extractionDest, "fixture", "iamadirectory", "another_file"))
 		Ω(err).ShouldNot(HaveOccurred())
 		Ω(string(fileContents)).Should(Equal("I am another file"))
 
-		f, err := os.Open(filepath.Join(destination, "fixture", "iamadirectory", "supervirus.exe"))
+		f, err := os.Open(filepath.Join(extractionDest, "fixture", "iamadirectory", "supervirus.exe"))
 		Ω(err).ShouldNot(HaveOccurred())
 
 		info, err := f.Stat()
 		Ω(err).ShouldNot(HaveOccurred())
 
 		Ω(info.Mode()).Should(Equal(os.FileMode(0755)))
-	})
+	}
 
-	It("should delete the zip file when its done", func() {
-		err := Extract(fixture, destination)
+	var cleanupTest = func() {
+		err := Extract(extractionSrc, extractionDest)
 		Ω(err).ShouldNot(HaveOccurred())
 
-		err = Extract(fixture, destination)
+		err = Extract(extractionSrc, extractionDest)
 		Ω(err).Should(HaveOccurred())
+	}
+
+	Context("when the file is a zip archive", func() {
+		BeforeEach(func() {
+			archiveFixture = "fixture.zip"
+		})
+
+		It("extracts the ZIP's files, generating directories, and honoring file permissions", func() {
+			extractionTest()
+		})
+
+		It("deletes the ZIP file when its done", func() {
+			cleanupTest()
+		})
+	})
+
+	Context("when the file is a tgz archive", func() {
+		BeforeEach(func() {
+			archiveFixture = "fixture.tgz"
+		})
+
+		It("extracts the TGZ's files, generating directories, and honoring file permissions", func() {
+			extractionTest()
+		})
+
+		It("deletes the TGZ file when its done", func() {
+			cleanupTest()
+		})
 	})
 })
