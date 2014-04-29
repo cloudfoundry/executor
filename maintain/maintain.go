64ca2c8fe211fd69d1c5acfc0efc6d67a10d60e2
Ted Young <tyoung@pivotallabs.com>
2014-04-28 17:48:04 -0700

    refactored maintaining presence into seperate package
    Signed-off-by: Chris Adams <cadams@pivotallabs.com>

---
+++ maintain/maintain.go
@@ -0,0 +1,57 @@
+package maintain
+
+import (
+	"errors"
+	"os"
+	"syscall"
+	"time"
+
+	Bbs "github.com/cloudfoundry-incubator/runtime-schema/bbs"
+	steno "github.com/cloudfoundry/gosteno"
+)
+
+type Maintainer struct {
+	id                string
+	bbs               Bbs.ExecutorBBS
+	logger            *steno.Logger
+	heartbeatInterval time.Duration
+}
+
+func New(id string, bbs Bbs.ExecutorBBS, logger *steno.Logger, heartbeatInterval time.Duration) *Maintainer {
+	return &Maintainer{
+		id:                id,
+		bbs:               bbs,
+		logger:            logger,
+		heartbeatInterval: heartbeatInterval,
+	}
+}
+
+func (m *Maintainer) Run(sigChan chan os.Signal) error {
+	presence, status, err := m.bbs.MaintainExecutorPresence(m.heartbeatInterval, m.id)
+	if err != nil {
+		m.logger.Errord(map[string]interface{}{
+			"error": err.Error(),
+		}, "executor.maintain_presence_begin.failed")
+	}
+
+	for {
+		select {
+		case sig := <-sigChan:
+			switch sig {
+			case syscall.SIGINT, syscall.SIGTERM:
+				presence.Remove()
+				return nil
+			}
+
+		case locked, ok := <-status:
+			if !ok {
+				return nil
+			}
+
+			if !locked {
+				m.logger.Error("executor.maintain_presence.failed")
+				return errors.New("Failed to maintain presence")
+			}
+		}
+	}
+}
