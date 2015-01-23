aa4067661af3aa6600f74f2a52faef565a1abcb5
Matthew Sykes <matthew.sykes@gmail.com>
2015-01-23 16:12:43 -0500

    Enable go generate for gardenstore.EventEmitter
    [#86668966]
    
    Signed-off-by: Michael Fraenkel <fraenkel@us.ibm.com>

--- depot/gardenstore/event_emitter.go
+++ depot/gardenstore/event_emitter.go
@@ -2,6 +2,7 @@ package gardenstore
 
 import "github.com/cloudfoundry-incubator/executor"
 
+//go:generate counterfeiter -o fakes/fake_event_emitter.go . EventEmitter
 type EventEmitter interface {
 	EmitEvent(executor.Event)
 }
