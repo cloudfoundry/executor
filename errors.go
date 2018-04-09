45186cb665fec66d95b47d71540e5c298f47b0bf
Will Pragnell <wpragnell@pivotal.io>
2018-04-09 15:30:00 -0700

    Remove unused http codes from errors

--- errors.go
+++ errors.go
@@ -1,18 +1,14 @@
 package executor
 
-import "net/http"
-
 type Error interface {
 	error
 
 	Name() string
-	HttpCode() int
 }
 
 type execError struct {
-	name     string
-	message  string
-	httpCode int
+	name    string
+	message string
 }
 
 func (err execError) Name() string {
@@ -23,28 +19,24 @@ func (err execError) Error() string {
 	return err.message
 }
 
-func (err execError) HttpCode() int {
-	return err.httpCode
-}
-
 var Errors = map[string]Error{}
 
-func registerError(name string, message string, status int) Error {
-	err := execError{name, message, status}
+func registerError(name, message string) Error {
+	err := execError{name, message}
 	Errors[name] = err
 	return err
 }
 
 var (
-	ErrContainerGuidNotAvailable      = registerError("ContainerGuidNotAvailable", "container guid not available", http.StatusBadRequest)
-	ErrContainerNotCompleted          = registerError("ContainerNotCompleted", "container must be stopped before it can be deleted", http.StatusBadRequest)
-	ErrInsufficientResourcesAvailable = registerError("InsufficientResourcesAvailable", "insufficient resources available", http.StatusServiceUnavailable)
-	ErrContainerNotFound              = registerError("ContainerNotFound", "container not found", http.StatusNotFound)
-	ErrStepsInvalid                   = registerError("StepsInvalid", "steps invalid", http.StatusBadRequest)
-	ErrLimitsInvalid                  = registerError("LimitsInvalid", "container limits invalid", http.StatusBadRequest)
-	ErrGuidNotSpecified               = registerError("GuidNotSpecified", "container guid not specified", http.StatusBadRequest)
-	ErrInvalidTransition              = registerError("InvalidStateTransition", "container cannot transition to given state", http.StatusConflict)
-	ErrFailureToCheckSpace            = registerError("ErrFailureToCheckSpace", "failed to check available space", http.StatusInternalServerError)
-	ErrInvalidSecurityGroup           = registerError("ErrInvalidSecurityGroup", "security group has invalid values", http.StatusBadRequest)
-	ErrNoProcessToStop                = registerError("ErrNoProcessToStop", "failed to find a process to stop", http.StatusNotFound)
+	ErrContainerGuidNotAvailable      = registerError("ContainerGuidNotAvailable", "container guid not available")
+	ErrContainerNotCompleted          = registerError("ContainerNotCompleted", "container must be stopped before it can be deleted")
+	ErrInsufficientResourcesAvailable = registerError("InsufficientResourcesAvailable", "insufficient resources available")
+	ErrContainerNotFound              = registerError("ContainerNotFound", "container not found")
+	ErrStepsInvalid                   = registerError("StepsInvalid", "steps invalid")
+	ErrLimitsInvalid                  = registerError("LimitsInvalid", "container limits invalid")
+	ErrGuidNotSpecified               = registerError("GuidNotSpecified", "container guid not specified")
+	ErrInvalidTransition              = registerError("InvalidStateTransition", "container cannot transition to given state")
+	ErrFailureToCheckSpace            = registerError("ErrFailureToCheckSpace", "failed to check available space")
+	ErrInvalidSecurityGroup           = registerError("ErrInvalidSecurityGroup", "security group has invalid values")
+	ErrNoProcessToStop                = registerError("ErrNoProcessToStop", "failed to find a process to stop")
 )
