c8c67ff999c7c36c11bd886dab6e5c141538ac78
John Shahid <jvshahid@gmail.com>
2018-07-10 10:29:33 -0400

    Sleep if the test disables goroutine leak detection
    this allows enough time for the goroutines to stabilize, so that the leaktest
    gets a consistent snapshot of the running goroutines in the BeforeEach of the
    following test.

--- depot/steps/steps_suite_test.go
+++ depot/steps/steps_suite_test.go
@@ -41,6 +41,12 @@ var _ = AfterEach(func() {
 	if checkGoroutines != nil {
 		time.AfterFunc(time.Second, cancel)
 		checkGoroutines()
+	} else {
+		// allow enough time for the goroutines to stabilize. Otherwise, sometimes
+		// new tests could start while goroutines are being created and the leaked
+		// goroutine is reported for an unrelated test. This is specially true for
+		// the monitor_test.go which disables goroutine checks.
+		time.Sleep(time.Second)
 	}
 
 	if err := goroutineErrors.ErrorOrNil(); err != nil {
