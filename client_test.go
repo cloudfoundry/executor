e3e14439814b42d89b0bc8e99439894b9384beca
Geoff Franks <gfranks@vmware.com>
2023-10-19 19:48:17 +0000

    Make go vet pass
    Signed-off-by: Josh Russett <jrussett@vmware.com>

--- client_test.go
+++ client_test.go
@@ -9,7 +9,7 @@ import (
 var _ = Describe("Allocation Request", func() {
 	It("is invalid when the guid is empty", func() {
 		allocationInfo := NewResource(20, 30, 1024)
-		allocRequest := NewAllocationRequest("", &allocationInfo, nil)
+		allocRequest := NewAllocationRequest("", &allocationInfo, false, nil)
 		err := allocRequest.Validate()
 		Expect(err).To(HaveOccurred())
 		Expect(err).To(MatchError(ErrGuidNotSpecified))
